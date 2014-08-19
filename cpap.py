6c7bba6c9074c7bc773dae6127c6ef4a195838c5
Ann Elliott <elliott.222@osu.edu>
2014-08-18 23:56:21 -0400

    Specify bin size on command line

--- cpap.py
+++ cpap.py
@@ -4,10 +4,11 @@ import csv
 import argparse
 import logging
 from collections import defaultdict, Counter
+from decimal import Decimal
 import datetime
 
 
-def main(input_file):
+def main(input_file, bin_size):
     data = []
     with open(input_file) as f:
         reader = csv.DictReader(f)
@@ -26,7 +27,7 @@ def main(input_file):
     for row in data:
         if row['Event'] == 'Pressure':
             timestamp = datetime.datetime.strptime(row['DateTime'], '%Y-%m-%dT%H:%M:%S')
-            pressure_bin = bin_pressure(row['Data/Duration'])
+            pressure_bin = bin_pressure(row['Data/Duration'], bin_size)
             if row['Session'] == current_session:
                 events_per_pressures[current_pressure_bin]['duration'] += timestamp - current_pressure_time
             current_pressure_bin = pressure_bin
@@ -41,14 +42,16 @@ def main(input_file):
         print key, value['duration'].total_seconds()/60., value['event_counts']
 
 
-def bin_pressure(pressure_string):
-    return pressure_string.split('.')[0]
+def bin_pressure(pressure_string, bin_size):
+    pressure = Decimal(pressure_string)
+    return (pressure // bin_size) * bin_size
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='From an input cpap log, find how many events of what type happened at which pressure ranges.')
     parser.add_argument(dest='input_file')
+    parser.add_argument('-b', '--binsize', dest='bin_size', default='.5', help='Bin size for pressures. Bin names are lower edge of range.')
     args = parser.parse_args()
-    main(args.input_file)
+    main(args.input_file, Decimal(args.bin_size))
 
 
